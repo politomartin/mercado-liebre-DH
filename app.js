de40acac0dfe43a89130966c122996697f48d68c
Martin <politomartin00@gmail.com>
2023-01-02 22:52:20 -0300

    Configuracion del puerto de variable de entorno

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.get('/login', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/login.html'))
 })
 
-app.listen(3030, () => {
-    console.log('Servidor iniciado en http://localhost:3030');
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log('Servidor iniciado en http://localhost:' + port);
 });
